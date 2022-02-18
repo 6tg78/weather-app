b48aab31854b741d96449cbd7ecaa95490bf528a
6tg78 <romek.gardys@gmail.com>
2022-02-18 12:44:38 +0100

    working weather app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,39 @@
 const searchForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+    const cityInfo = data.cityInfo;
+    const weather = data.weather;
+
+    details.innerHTML = `
+    <h5 class="my-3">${cityInfo.EnglishName}</h5>
+    <div class="my-3">At the moment: ${weather.WeatherText.toLowerCase()}</div>
+    <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+    `;
+
+    if (card.classList.contains('d-none')) {
+        card.classList.remove('d-none');
+    }
+};
+
+const updateCity = async (city) => {
+    const cityInfo = await getCity(city);
+    const weather = await getWeather(cityInfo.Key);
+
+    return { cityInfo, weather };
+};
 
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    //getting the value from the form
     const city = searchForm.city.value.trim();
     searchForm.reset();
-    console.log(city);
+
+    updateCity(city)
+        .then((data) => updateUI(data))
+        .catch((err) => console.log(err));
 });
