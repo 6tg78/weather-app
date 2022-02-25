92dfcc1521ed40d6141583c45ab01f0af72811d9
6tg78 <romek.gardys@gmail.com>
2022-02-25 19:11:43 +0100

    update

--- src/app.js
+++ src/app.js
@@ -1,38 +1,39 @@
 const searchForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const timeOfDay = document.querySelector('img.time');
 
 const updateUI = (data) => {
-    const { cityInfo, weather } = data;
+  const { cityInfo, weather } = data;
 
-    details.innerHTML = `
+  details.innerHTML = `
     <h5 class="my-3">${cityInfo.EnglishName}</h5>
-    <div class="my-3">At the moment: ${weather.WeatherText.toLowerCase()}</div>
+    <div class="my-3 text-info">At the moment: ${weather.WeatherText.toLowerCase()}</div>
     <div class="display-4 my-4">
         <span>${weather.Temperature.Metric.Value}</span>
         <span>&deg;C</span>
     `;
 
-    if (card.classList.contains('d-none')) {
-        card.classList.remove('d-none');
-    }
+  if (card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  }
 };
 
 const updateCity = async (city) => {
-    const cityInfo = await getCity(city);
-    const weather = await getWeather(cityInfo.Key);
+  const cityInfo = await getCity(city);
+  const weather = await getWeather(cityInfo.Key);
 
-    return { cityInfo, weather };
+  return { cityInfo, weather };
 };
 
 searchForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    //getting the value from the form
-    const city = searchForm.city.value.trim();
-    searchForm.reset();
+  //getting the value from the form
+  const city = searchForm.city.value.trim();
+  searchForm.reset();
 
-    updateCity(city)
-        .then((data) => updateUI(data))
-        .catch((err) => console.log(err));
+  updateCity(city)
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err));
 });
--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -2,27 +2,26 @@ const APIkey = 'DIfdvI7iXSz9SMOgmaLf0GE55P5exI0k';
 
 //get current conditions information
 const getWeather = async (id) => {
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-    const query = `${id}?apikey=${APIkey}`;
+  const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${APIkey}`;
 
-    const response = await fetch(base + query);
-    const data = await response.json();
-    return data[0];
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
 };
 
 // get city information
 const getCity = async (city) => {
-    const base =
-        'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apikey=${APIkey}&q=${city}`;
+  const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${APIkey}&q=${city}`;
 
-    const response = await fetch(base + query);
-    const data = await response.json();
-    return data[0];
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
 };
 
 getCity('warsaw')
-    .then((data) => {
-        return getWeather(data.Key);
-    })
-    .catch((err) => console.log(err));
+  .then((data) => {
+    return getWeather(data.Key);
+  })
+  .catch((err) => console.log(err));
